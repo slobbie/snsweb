37ae647a48d0acc025878345e34edc26bf10383a
slobbie <haeru9410@gmail.com>
2022-02-08 01:29:06 +0900

    logout 기능 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from '../routers/Auth';
 import Home from '../routers/Home';
+import Profile from '../routers/Profile';
+
+import Nav from './Nav';
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <>
+      {isLoggedIn && <Nav />}
       {isLoggedIn ? (
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/profile' element={<Profile />} />
         </Routes>
       ) : (
         <Routes>
--- src/routers/Profile.js
+++ src/routers/Profile.js
@@ -1,3 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { authService } from '../fireinst';
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate('/'); // logout 시에 메인으로 이동
+  };
+  return (
+    <div>
+      <span>Profile</span>
+      <button onClick={onLogOutClick}>Log out</button>
+    </div>
+  );
+};
+
+export default Profile;
