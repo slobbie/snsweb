891973a554bf7e1ed6ac154775fb4c3b2c862706
slobbie <haeru9410@gmail.com>
2022-02-07 23:28:17 +0900

    auth 기본 프레임 layout ,  input value 설정

--- src/routers/Auth.js
+++ src/routers/Auth.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <span>Auth</span>;
+      <form onSubmit={onSubmit}>
+        <input
+          name='email'
+          type='text'
+          placeholder='Email'
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type='submit' value='Log In' />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+      </div>
     </div>
   );
 };
