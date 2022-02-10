c7560b4cb531417d597b9133721ad50ca96cf36b
slobbie <haeru9410@gmail.com>
2022-02-10 23:58:49 +0900

    수정 기능 지우기 기능 구현

--- src/components/peed.js
+++ src/components/peed.js
@@ -1,11 +1,56 @@
+import { useState } from 'react';
+import { dbService } from '../fireinst';
+
 const Peed = ({ peedObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newPeed, setNewPeed] = useState(peedObj.text);
+  const onDeleteClick = async () => {
+    const ok = window.confirm('게시글을 지우겠습니까?');
+    console.log(ok);
+    if (ok) {
+      await dbService.doc(`peeds/${peedObj.id}`).delete();
+    }
+  };
+  const toggleEditing = () => {
+    setEditing(!editing);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.doc(`peeds/${peedObj.id}`).update({
+      text: newPeed,
+    });
+    setEditing(false);
+  };
+
+  const onChange = (e) => {
+    setNewPeed(e.target.value);
+  };
   return (
     <div key={peedObj.id}>
-      <h4>{peedObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Dekete Peed</button>
-          <button>Edit Peed</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type='text'
+              placeholder='새로운 글을 작성해주세요.'
+              defaultValue={newPeed}
+              required
+              onChange={onChange}
+            />
+            <input type='submit' value='수정' />
+          </form>
+          <button onClick={toggleEditing}>취소</button>
+        </>
+      ) : (
+        <>
+          <h4>{peedObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Peed</button>
+              <button onClick={toggleEditing}>Edit Peed</button>
+            </>
+          )}
         </>
       )}
     </div>
